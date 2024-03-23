3bb03dac00908b19f15a55b2dba157f4ec7cf1ec
mucahitsimsek <mucahitsmsk74@gmail.com>
2024-03-24 00:06:40 +0300

    Module folder and translations creating

--- module/gen/lib/src/asset/assets.gen.dart
+++ module/gen/lib/src/asset/assets.gen.dart
@@ -7,6 +7,10 @@
 // ignore_for_file: type=lint
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/services.dart';
+
 class $AssetsColorGen {
   const $AssetsColorGen();
 
@@ -17,10 +21,319 @@ class $AssetsColorGen {
   List<String> get values => [colors];
 }
 
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/back_icon.svg
+  SvgGenImage get backIcon => const SvgGenImage('assets/icons/back_icon.svg');
+
+  /// File path: assets/icons/camera_icon.svg
+  SvgGenImage get cameraIcon =>
+      const SvgGenImage('assets/icons/camera_icon.svg');
+
+  /// File path: assets/icons/card_icon.svg
+  SvgGenImage get cardIcon => const SvgGenImage('assets/icons/card_icon.svg');
+
+  /// File path: assets/icons/clock_icon.svg
+  SvgGenImage get clockIcon => const SvgGenImage('assets/icons/clock_icon.svg');
+
+  /// File path: assets/icons/close_icon.svg
+  SvgGenImage get closeIcon => const SvgGenImage('assets/icons/close_icon.svg');
+
+  /// File path: assets/icons/delivery_icon.svg
+  SvgGenImage get deliveryIcon =>
+      const SvgGenImage('assets/icons/delivery_icon.svg');
+
+  /// File path: assets/icons/document_icon.svg
+  SvgGenImage get documentIcon =>
+      const SvgGenImage('assets/icons/document_icon.svg');
+
+  /// File path: assets/icons/done_icon.svg
+  SvgGenImage get doneIcon => const SvgGenImage('assets/icons/done_icon.svg');
+
+  /// File path: assets/icons/dropdown_icon.svg
+  SvgGenImage get dropdownIcon =>
+      const SvgGenImage('assets/icons/dropdown_icon.svg');
+
+  /// File path: assets/icons/facebook_icon.svg
+  SvgGenImage get facebookIcon =>
+      const SvgGenImage('assets/icons/facebook_icon.svg');
+
+  /// File path: assets/icons/fast_delivery_icon.svg
+  SvgGenImage get fastDeliveryIcon =>
+      const SvgGenImage('assets/icons/fast_delivery_icon.svg');
+
+  /// File path: assets/icons/forward_icon.svg
+  SvgGenImage get forwardIcon =>
+      const SvgGenImage('assets/icons/forward_icon.svg');
+
+  /// File path: assets/icons/google_icon.svg
+  SvgGenImage get googleIcon =>
+      const SvgGenImage('assets/icons/google_icon.svg');
+
+  /// File path: assets/icons/home_icon.svg
+  SvgGenImage get homeIcon => const SvgGenImage('assets/icons/home_icon.svg');
+
+  /// File path: assets/icons/intro_first_icon.svg
+  SvgGenImage get introFirstIcon =>
+      const SvgGenImage('assets/icons/intro_first_icon.svg');
+
+  /// File path: assets/icons/intro_second_icon.svg
+  SvgGenImage get introSecondIcon =>
+      const SvgGenImage('assets/icons/intro_second_icon.svg');
+
+  /// File path: assets/icons/intro_thirty_icon.svg
+  SvgGenImage get introThirtyIcon =>
+      const SvgGenImage('assets/icons/intro_thirty_icon.svg');
+
+  /// File path: assets/icons/location_icon.svg
+  SvgGenImage get locationIcon =>
+      const SvgGenImage('assets/icons/location_icon.svg');
+
+  /// File path: assets/icons/lock_icon.svg
+  SvgGenImage get lockIcon => const SvgGenImage('assets/icons/lock_icon.svg');
+
+  /// File path: assets/icons/logo_icon.svg
+  SvgGenImage get logoIcon => const SvgGenImage('assets/icons/logo_icon.svg');
+
+  /// File path: assets/icons/logout_icon.svg
+  SvgGenImage get logoutIcon =>
+      const SvgGenImage('assets/icons/logout_icon.svg');
+
+  /// File path: assets/icons/marker_icon.svg
+  SvgGenImage get markerIcon =>
+      const SvgGenImage('assets/icons/marker_icon.svg');
+
+  /// File path: assets/icons/minus_icon.svg
+  SvgGenImage get minusIcon => const SvgGenImage('assets/icons/minus_icon.svg');
+
+  /// File path: assets/icons/notification_icon.svg
+  SvgGenImage get notificationIcon =>
+      const SvgGenImage('assets/icons/notification_icon.svg');
+
+  /// File path: assets/icons/orders_icon.svg
+  SvgGenImage get ordersIcon =>
+      const SvgGenImage('assets/icons/orders_icon.svg');
+
+  /// File path: assets/icons/plus_icon.svg
+  SvgGenImage get plusIcon => const SvgGenImage('assets/icons/plus_icon.svg');
+
+  /// File path: assets/icons/profile_icon.svg
+  SvgGenImage get profileIcon =>
+      const SvgGenImage('assets/icons/profile_icon.svg');
+
+  /// File path: assets/icons/rating_icon.svg
+  SvgGenImage get ratingIcon =>
+      const SvgGenImage('assets/icons/rating_icon.svg');
+
+  /// File path: assets/icons/reset_email_icon.svg
+  SvgGenImage get resetEmailIcon =>
+      const SvgGenImage('assets/icons/reset_email_icon.svg');
+
+  /// File path: assets/icons/rounded_close_icon.svg
+  SvgGenImage get roundedCloseIcon =>
+      const SvgGenImage('assets/icons/rounded_close_icon.svg');
+
+  /// File path: assets/icons/search_icon.svg
+  SvgGenImage get searchIcon =>
+      const SvgGenImage('assets/icons/search_icon.svg');
+
+  /// File path: assets/icons/share_icon.svg
+  SvgGenImage get shareIcon => const SvgGenImage('assets/icons/share_icon.svg');
+
+  /// File path: assets/icons/welcome_ice_cream_icon.svg
+  SvgGenImage get welcomeIceCreamIcon =>
+      const SvgGenImage('assets/icons/welcome_ice_cream_icon.svg');
+
+  /// List of all assets
+  List<SvgGenImage> get values => [
+        backIcon,
+        cameraIcon,
+        cardIcon,
+        clockIcon,
+        closeIcon,
+        deliveryIcon,
+        documentIcon,
+        doneIcon,
+        dropdownIcon,
+        facebookIcon,
+        fastDeliveryIcon,
+        forwardIcon,
+        googleIcon,
+        homeIcon,
+        introFirstIcon,
+        introSecondIcon,
+        introThirtyIcon,
+        locationIcon,
+        lockIcon,
+        logoIcon,
+        logoutIcon,
+        markerIcon,
+        minusIcon,
+        notificationIcon,
+        ordersIcon,
+        plusIcon,
+        profileIcon,
+        ratingIcon,
+        resetEmailIcon,
+        roundedCloseIcon,
+        searchIcon,
+        shareIcon,
+        welcomeIceCreamIcon
+      ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/dummy_food.png
+  AssetGenImage get dummyFood =>
+      const AssetGenImage('assets/images/dummy_food.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [dummyFood];
+}
+
 class Assets {
   Assets._();
 
   static const String package = 'gen';
 
   static const $AssetsColorGen color = $AssetsColorGen();
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName);
+
+  final String _assetName;
+
+  static const String package = 'gen';
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    @Deprecated('Do not specify package for a generated library asset')
+    String? package = package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    @Deprecated('Do not specify package for a generated library asset')
+    String? package = package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => 'packages/gen/$_assetName';
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName);
+
+  final String _assetName;
+
+  static const String package = 'gen';
+
+  SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    @Deprecated('Do not specify package for a generated library asset')
+    String? package = package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    SvgTheme theme = const SvgTheme(),
+    ColorFilter? colorFilter,
+    Clip clipBehavior = Clip.hardEdge,
+    @deprecated Color? color,
+    @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
+    @deprecated bool cacheColorFilter = false,
+  }) {
+    return SvgPicture.asset(
+      _assetName,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      bundle: bundle,
+      package: package,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      theme: theme,
+      colorFilter: colorFilter,
+      color: color,
+      colorBlendMode: colorBlendMode,
+      clipBehavior: clipBehavior,
+      cacheColorFilter: cacheColorFilter,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => 'packages/gen/$_assetName';
 }
--- module/gen/lib/src/asset/colors.gen.dart
+++ module/gen/lib/src/asset/colors.gen.dart
@@ -13,84 +13,30 @@ import 'package:flutter/material.dart';
 class ColorName {
   ColorName._();
 
-  /// Color: #000000
-  static const Color black = Color(0xFF000000);
+  /// Color: #F8B64C
+  static const Color accentColor = Color(0xFFF8B64C);
 
-  /// MaterialColor:
-  ///   50: #FFF9E5E5
-  ///   100: #FFF1BFBF
-  ///   200: #FFE79595
-  ///   300: #FFDD6A6A
-  ///   400: #FFD64A4A
-  ///   500: #FFCF2A2A
-  ///   600: #FFCA2525
-  ///   700: #FFC31F1F
-  ///   800: #FFBD1919
-  ///   900: #FFB20F0F
-  static const MaterialColor crimsonRed = MaterialColor(
-    0xFFCF2A2A,
-    <int, Color>{
-      50: Color(0xFFF9E5E5),
-      100: Color(0xFFF1BFBF),
-      200: Color(0xFFE79595),
-      300: Color(0xFFDD6A6A),
-      400: Color(0xFFD64A4A),
-      500: Color(0xFFCF2A2A),
-      600: Color(0xFFCA2525),
-      700: Color(0xFFC31F1F),
-      800: Color(0xFFBD1919),
-      900: Color(0xFFB20F0F),
-    },
-  );
-
-  /// Color: #979797
-  static const Color gray410 = Color(0xFF979797);
-
-  /// Color: #EEEEEE
-  static const Color gray70 = Color(0xFFEEEEEE);
+  /// Color: #EEA734
+  static const Color activeColorOne = Color(0xFFEEA734);
 
   /// Color: #FFFFFF
-  static const Color white = Color(0xFFFFFFFF);
+  static const Color backgroundColor = Color(0xFFFFFFFF);
 
-  /// MaterialColor:
-  ///   50: #FFFBF2E5
-  ///   100: #FFF5DFBE
-  ///   200: #FFEFCA93
-  ///   300: #FFE9B568
-  ///   400: #FFE4A547
-  ///   500: #FFDF9527
-  ///   600: #FFDB8D23
-  ///   700: #FFD7821D
-  ///   800: #FFD27817
-  ///   900: #FFCA670E
-  static const MaterialColor yellowOcher = MaterialColor(
-    0xFFDF9527,
-    <int, Color>{
-      50: Color(0xFFFBF2E5),
-      100: Color(0xFFF5DFBE),
-      200: Color(0xFFEFCA93),
-      300: Color(0xFFE9B568),
-      400: Color(0xFFE4A547),
-      500: Color(0xFFDF9527),
-      600: Color(0xFFDB8D23),
-      700: Color(0xFFD7821D),
-      800: Color(0xFFD27817),
-      900: Color(0xFFCA670E),
-    },
-  );
+  /// Color: #868686
+  static const Color bodyText = Color(0xFF868686);
 
-  /// MaterialAccentColor:
-  ///   100: #FFFFE8E0
-  ///   200: #FFFFBCA3
-  ///   400: #FFFFA989
-  ///   700: #FFFF9E7A
-  static const MaterialAccentColor yellowOcherAccent = MaterialAccentColor(
-    0xFFFFBCA3,
-    <int, Color>{
-      100: Color(0xFFFFE8E0),
-      200: Color(0xFFFFBCA3),
-      400: Color(0xFFFFA989),
-      700: Color(0xFFFF9E7A),
-    },
-  );
+  /// Color: #22A45D
+  static const Color green = Color(0xFF22A45D);
+
+  /// Color: #FBFBFB
+  static const Color inputColor = Color(0xFFFBFBFB);
+
+  /// Color: #010F07
+  static const Color mainColorBlack = Color(0xFF010F07);
+
+  /// Color: #FF7058
+  static const Color red = Color(0xFFFF7058);
+
+  /// Color: #FFD15C
+  static const Color yellow = Color(0xFFFFD15C);
 }
